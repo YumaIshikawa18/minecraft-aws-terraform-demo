64e0dfcc73b1478604a61db07ff74706e0e17e71
YumaIshikawa18 <ishikawa_yuma@ims-sol.co.jp>
2026-01-13 18:45:58 +0900

    fix workflow

--- environment/backend.tf
+++ environment/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "YOUR_TFSTATE_BUCKET_NAME"
+    bucket = "yuma-minecraft-tfstate"
     key    = "minecraft/terraform.tfstate"
     region = "ap-northeast-1"
   }
